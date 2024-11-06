5c0e59a4a5672fe382f666674c9da36dcc07bddc
Paul Blair <blairpaul8@yahoo.com>
2024-11-06 11:26:43 -0500

    Got functions working. Added a initialize, write_character, and print function.

--- arduino_bare_metal/lcd/lcd.c
+++ arduino_bare_metal/lcd/lcd.c
@@ -1,18 +1,20 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <string.h>
 
 // All Commands for the LCD
 #define LCD_CLEAR 0x01
 #define LCD_ENRTY_MODE 0x06
 #define LCD_ON 0x0F
 
-#define PULSE_ENABLE()                                                         \
-  PORTB |= (1 << PORTB3);                                                      \
-  _delay_us(2);                                                                \
-  PORTB &= ~(1 << PORTB3);                                                     \
+void pulse_enable() {
+  PORTB |= (1 << PORTB3);                                                      
+  _delay_us(2);                                                                
+  PORTB &= ~(1 << PORTB3);                                                     
   _delay_ms(2);
+}
 
-int main(void) {
+void lcd_init() {
   // Set all DDRD pins to 1 for output
   DDRD = 0xFF;
   // Set all PORTD pins to 0
@@ -28,30 +30,47 @@ int main(void) {
   _delay_ms(15);
   // clear the display
   PORTD = 0x01;
-  PORTB |= (1 << PORTB3);
-  _delay_us(2);
-  PORTB &= ~(1 << PORTB3);
-  _delay_ms(2);
+  pulse_enable();
 
   // Entry mode set move cursor right upon writing character
   PORTD = 0x06;
-  PORTB |= (1 << PORTB3);
-  _delay_us(2);
-  PORTB &= ~(1 << PORTB3);
-  _delay_ms(2);
+  pulse_enable();
 
   // Turn display on
   PORTD = 0x0F;
+  pulse_enable();
+
+}
+
+void second_row() {
+  PORTD = 0x40;
   PORTB |= (1 << PORTB3);
   _delay_us(2);
   PORTB &= ~(1 << PORTB3);
   _delay_ms(2);
+}
 
+
+
+void write_character(uint8_t c) {
   PORTB |= (1 << PORTB4);
-  PORTD = 0x41;
-  /* PORTB |= (1 << PORTB3);
-   _delay_us(2);
-   PORTB &= ~(1 << PORTB3);
-   _delay_ms(2);*/
-  PULSE_ENABLE();
+  PORTD = c;
+  pulse_enable();
+  PORTB &= ~(1 << PORTB4);
+  _delay_ms(2);
+}
+
+void lcd_print(char *c) {
+  for (int i = 0; i < strlen(c); i++) {
+    write_character(c[i]);
+  }
+
+}
+
+int main(void) {
+  //initialize the lcd
+  lcd_init();
+  //char *c = "Hello World";
+  lcd_print("Hello World");
+  
 }
